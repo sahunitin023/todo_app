bc7fd68b90bf06b74a68ef8757c60c6cfcf40c33
sahunitin023 <b521039@iiit-bh.ac.in>
2023-01-23 14:58:29 +0530

    Designed the Bottom Sheet UI

--- lib/screeens/add_task_screen.dart
+++ lib/screeens/add_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_app/constants.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({Key? key}) : super(key: key);
@@ -9,7 +10,65 @@ class AddTaskScreen extends StatelessWidget {
       decoration: const BoxDecoration(
         color: Color.fromARGB(255, 14, 14, 14),
       ),
-      child: Container(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 40.0),
+        decoration: const BoxDecoration(
+          color: kSecondaryColor1,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: kPrimaryColor,
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const TextField(
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: 'Enter Task',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: kPrimaryColor,
+                    width: 10,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.circular(30.0),
+                child: const MaterialButton(
+                  onPressed: null,
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Add',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
